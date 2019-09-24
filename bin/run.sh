1a41c5369b8a92903a056d4965978a61b8e17041
Marek Wiewiórka <marek.wiewiorka@gmail.com>
2019-09-24 07:37:09 +0200

    Docker image

--- bin/run.sh
+++ bin/run.sh
@@ -1,2 +1,4 @@
 #!/bin/bash -x
-Rscript -e "library(knitr); knit('performance.Rmd')"
+#Rscript -e "library(knitr); knit('performance.Rmd')"
+
+docker run --rm -it -v $PWD:/tmp -v $1:/data biodatageeks/perf-report-generator:0.1 sh -c "cd /tmp && Rscript -e \"library(knitr); rmarkdown::render('performance.Rmd')\""
